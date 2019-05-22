acd01e6349098188de368e690335de6495129e8c
Max van den Berk <mvandenberk@conclusion.nl>
2019-05-22 12:51:11 +0200

    logger added

--- MongoHandler.js
+++ MongoHandler.js
@@ -29,7 +29,9 @@ async function Controller(Obj, Class, res) {
         if (error) {
             return console.dir(error);
         }
+        console.log("body to be added to Response.Image "body)
         Response.Image = JSON.parse(body);
+        
     })
 
     let CountObject = await Request.get(mongoURL);
